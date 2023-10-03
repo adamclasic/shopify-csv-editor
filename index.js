49672cdf09c4cad668d6a28aa2fece479a8a5b43
adamclasic <aabou9@gmail.com>
2023-10-03 16:52:34 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const fs = require("fs");
+const fs = require('fs');
 
-const { parse: parse2json } = require("csv-parse/sync");
-const { parse: parse2csv } = require("json2csv");
+const { parse: parse2json } = require('csv-parse/sync');
+const { parse: parse2csv } = require('json2csv');
 
 const shopifyFinal = [];
 let tempImagesArr = [];
-const generalSneakersData = fs.readFileSync("./src/generalSneakers.csv", {
-  encoding: "utf8",
-  flag: "r",
+
+const generalSneakersData = fs.readFileSync('./src/generalSneakers.csv', {
+  encoding: 'utf8',
+  flag: 'r',
+});
+
+const generalAllData = fs.readFileSync('./src/generalAllTrue.csv', {
+  encoding: 'utf8',
+  flag: 'r',
 });
 
 const generalSneakers = parse2json(generalSneakersData, {
@@ -16,9 +22,9 @@ const generalSneakers = parse2json(generalSneakersData, {
 });
 // console.log(generalSneakers[1]);
 
-const shopifyAllData = fs.readFileSync("./src/shopifyAll.csv", {
-  encoding: "utf8",
-  flag: "r",
+const shopifyAllData = fs.readFileSync('./src/shopifyAll.csv', {
+  encoding: 'utf8',
+  flag: 'r',
 });
 
 const shopifyAll = parse2json(shopifyAllData, {
@@ -29,19 +35,19 @@ const shopifyAll = parse2json(shopifyAllData, {
 
 shopifyAll.forEach((initSho) => {
   generalSneakers.forEach((geneElm) => {
-    if (geneElm.id == initSho.Handle.split("-")[1]) {
-      // console.log(`row.id ${geneElm.id} is in both files, `);
+    if (geneElm.id == initSho.Handle.split('-')[1]) {
+      console.log(`row.id ${geneElm.id} is in both files, collection name is: ${geneElm.model} `);
 
-      initSho["Collection"] = "MC SNEAKERS";
+      initSho['Collection'] = 'MC GOLF SHOES'; // Add Collection (Optional)
       shopifyFinal.push(initSho);
-      tempImagesArr = geneElm.images.split(",");
+      tempImagesArr = geneElm.images.split(',');
       if (tempImagesArr.length > 1) {
-        console.log("has " + tempImagesArr.length + " images");
+        console.log('has ' + tempImagesArr.length + ' images');
         tempImagesArr.forEach((imgSrc) => {
-          if (imgSrc === initSho["Image Src"]) return;
+          if (imgSrc === initSho['Image Src']) return;
           shopifyFinal.push({
             Handle: initSho.Handle,
-            "Image Src": imgSrc,
+            'Image Src': imgSrc,
           });
         });
       }
@@ -52,9 +58,10 @@ shopifyAll.forEach((initSho) => {
 const csv = parse2csv(shopifyFinal);
 
 try {
-  fs.appendFileSync("./output1.0.2.csv", csv);
+  fs.appendFileSync('./output.MensDressGolf.3.csv', csv);
 } catch (err) {
   console.error(err);
 }
 
 console.log(csv);
+// Warning can not use multiple times in one file as it creates new header colums every time
